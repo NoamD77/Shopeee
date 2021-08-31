32235f6ea571c0148d9eea75ae40d42d0cda94df
Almog <almogss15@gmail.com>
2021-08-31 18:41:37 +0300

    change eMail to Email on create user function

--- Shopeee/Controllers/UsersController.cs
+++ Shopeee/Controllers/UsersController.cs
@@ -76,7 +76,7 @@ namespace Shopeee.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserName,Password,eMail,Permissions")] User user)
+        public async Task<IActionResult> Create([Bind("Id,UserName,Password,Email,Permissions")] User user)
         {
             if (ModelState.IsValid)
             {
