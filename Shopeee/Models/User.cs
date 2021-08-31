32bfb49ad5a0d603a5e4277c56564902d0923131
Noam Dadush <noamd7755@gmail.com>
2021-08-31 20:03:34 +0300

    User changes

--- Shopeee/Models/User.cs
+++ Shopeee/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,11 +10,30 @@ namespace Shopeee.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
         public String FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
         public String LastName { get; set; }
+        [Required]
+        [Display(Name = "User Name")]
         public String UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public String Password { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
         public String Email { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+
+
         public String Permissions { get; set; }
     }
 }
