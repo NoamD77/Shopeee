97ec424c4055dcd323b868987eac9e14ae356399
Almog <almogss15@gmail.com>
2021-11-05 12:59:27 +0200

    add search and filter options for brands and branches

---
+++ Shopeee/Types/AreaType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopeee.Models
+{
+    public enum AreaType
+    {
+        Center = 0,
+        North = 1,
+        South = 2,
+        //continue adding
+    }
+}
