e5e69ec9955e55b8b413ed48dc60f83b658d61df
Stav Gabay <stavgab@cs.colman.ac.il>
2021-11-14 17:30:31 +0200

    added data

--- Shopeee/wwwroot/js/PieChart.js
+++ Shopeee/wwwroot/js/PieChart.js
@@ -11,7 +11,7 @@
 
 
     var color = d3.scale.ordinal()
-        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#ff8c11"]);
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#ff8c11","#aa77bb","#89a3bf"]);
 
 
     var arc = d3.svg.arc()
